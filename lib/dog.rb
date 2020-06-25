a1212a689fd7822730f8ce35d525ef5e53564379
Jessica Pereira <jcpereira070@gmail.com>
2020-06-25 04:04:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
 class Dog
-
-  @@all = []
-  attr_accessor :name
-
+attr_accessor :name
+@@all = []
   def initialize(name)
     @name = name
-    @@all << self
-    # @clear_all = clear_all
+    @save = save
   end
   def self.all
     @@all
@@ -14,13 +11,13 @@ class Dog
   def self.clear_all
     @@all.clear
   end
-
-  private
-
-  def self.print_all
-    my_array = @@all.map do |dog|
-    dog.name
+# # private
+def self.print_all
+  @@all.each do |dogs|
+    puts dogs.name
   end
-  puts my_array
+end
+def save
+  @@all << self
 end
 end
